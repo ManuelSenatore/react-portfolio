24b9656fe20f24373e50db4bc41d978a4353ead5
ManuelSenatore <106093444+ManuelSenatore@users.noreply.github.com>
2023-01-05 17:26:17 +0100

    home

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-
-import './App.scss';
-import { Route, Routes } from 'react-router-dom';
-import Layout from './components/Layout';
+import './App.scss'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './components/Layout'
+import Home from './components/Home'
 
 function App() {
   return (
-  <>
-  <Routes>
-    <Route path="/" element={<Layout />} />
-  </Routes>
-  </>
-  );
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </>
+  )
 }
 
-export default App;
+export default App
