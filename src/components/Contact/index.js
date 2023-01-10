bdfe5cb6a37dd865c09b9c0ffd9bdbb4d90f6bab
ManuelSenatore <106093444+ManuelSenatore@users.noreply.github.com>
2023-01-10 12:57:35 +0100

    map

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,37 @@
 import './index.scss'
 import { Loader } from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import emailjs from '@emailjs/browser';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const Contact = () => {
   const [letterClass, setLetterClass] = useState('text-animate')
+  const refForm = useRef()
 
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
     }, 3000)
   }, [])
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm(
+      'service_tn4vjlc',
+      'template_ys1n1yk',
+      refForm.current,
+      'mrSJ6T9l2inTozUjq'
+    )
+    .then( () => {
+      alert('Message successfully sent')
+      window.location.reload(false)
+    },
+    () => {
+      alert('Failed to send the message')
+    })
+  }
   return (
     <>
       <div className="container contact-page">
@@ -28,16 +48,13 @@ const Contact = () => {
             However, if you have other request or question, don't hesitate to contact me using below form either.
           </p>
           <div className='contact-form'>
-            <form>
+            <form ref={refForm} onSubmit={sendEmail}>
                 <ul>
                     <li className='half'>
-                        <input type='text' name='name' placeholder='Name' required />
+                        <input type='text' name='user_name' placeholder='Name' required />
                     </li>
                     <li className='half'>
-                        <input type='email' name='email' placeholder='Email' required />
-                    </li>
-                    <li>
-                        <input placeholder='Subject' type="text" name='subject' required />
+                        <input type='email' name='user_email' placeholder='Email' required />
                     </li>
                     <li>
                         <textarea placeholder='Message' name='message' required></textarea>
@@ -46,6 +63,22 @@ const Contact = () => {
                 </ul>
             </form>
           </div>
+        </div>
+        <div className='info-map'>
+        Manuel Senatore,
+        <br />
+        Italy,
+        <br />
+        Cava De Tirreni 84013 SA <br/>
+        <span>manuelsenatore8@gmail.com</span>
+        </div>
+        <div className='map-wrap'>
+          <MapContainer center={[40.723224, 14.695919]} zoom={13}>
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+          <Marker position={[40.723224, 14.695919]} >
+            <Popup> Manuel lives here, come over for a cup of coffee :)</Popup>
+          </Marker>
+          </MapContainer>
         </div>
       </div>
       <Loader type="pacman" />
